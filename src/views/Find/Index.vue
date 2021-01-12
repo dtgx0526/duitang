2ca345027de87ab2e25663d53309b124f9429bad
xiaobai <735103914>
2021-01-12 21:01:43 +0800

    second day

---
+++ src/views/Find/Index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>发现</p>
+        <img src="//s.syzs.qq.com/gameassistant/web/0c4b2a0dd65c08aa94ce8979cf4bf5e2.jpg.big.webp" alt="">
+        <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F201903%2F10%2F20190310171650_rnklq.thumb.400_0.jpeg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612947438&t=1213cccc122163b31bdd24578f924da7" alt="">
+        <img src="https://ss1.baidu.com/9vo3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/8718367adab44aed57c11f92b11c8701a18bfb3d.jpg" alt="">
+        <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F8dacd461cab76ff0ce00979adadae73e0dfa32cc.jpg&refer=http%3A%2F%2Fi0.hdslb.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612947928&t=27297b7ac63041a67910713b4cd2c8b8" alt="">
+        <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage.9game.cn%2F2019%2F3%2F11%2F61385269.jpg&refer=http%3A%2F%2Fimage.9game.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612948037&t=69b07197f35b8307e0fe6645ea72f6a9" alt="">
+    </div>
+    
+</template>
+<style lang="scss"  scoped>
+p{
+    text-align: center;
+    padding-bottom: 3px;
+}
+    img{
+        width: 100%;
+        border-radius: 20px 30px;
+        padding-bottom: 5px;
+        height: 222.66px;
+    }
+</style>
